e1758b1a56f11b00975e43c4dc297ab5165d69a5
Eric Alessandrini <eric.alessandrini1@gmail.com>
2024-03-06 12:25:12 -0300

    Iniciando repositorio del proyecto StudentManagementSystem

---
+++ src/main/java/com/ega/sms/SmsController.java
@@ -0,0 +1,14 @@
+package com.ega.sms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SmsController {
+
+	@GetMapping("/")
+	public String paginaInicio() {
+		return "index";
+	}
+}
---
+++ src/test/java/com/ega/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ega.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
